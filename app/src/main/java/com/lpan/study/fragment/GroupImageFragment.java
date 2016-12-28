76d974a739460c473575d6cd35ef67a93be5d66e
lpan <jsonlp110@gmail.com>
2016-12-28 14:42:57 +0800

    add group avatar image

--- app/src/main/java/com/lpan/study/fragment/GroupImageFragment.java
+++ app/src/main/java/com/lpan/study/fragment/GroupImageFragment.java
@@ -4,23 +4,28 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.View;
 
-import com.lpan.study.view.CircleImnageView;
-import com.lpan.study.view.GroupImageView;
+import com.lpan.study.view.CircularImageView;
 import com.test.lpanstudyrecord.R;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by lpan on 2016/12/27.
  */
 public class GroupImageFragment extends BaseFragment {
 
-    private CircleImnageView mCircleImnageView;
 
-    private GroupImageView mGroupImageView;
+    ArrayList<Bitmap> mBmps1 = new ArrayList<Bitmap>();
+    ArrayList<Bitmap> mBmps2 = new ArrayList<Bitmap>();
+    ArrayList<Bitmap> mBmps3 = new ArrayList<Bitmap>();
+    ArrayList<Bitmap> mBmps4 = new ArrayList<Bitmap>();
+    ArrayList<Bitmap> mBmps5 = new ArrayList<Bitmap>();
 
-
+    CircularImageView mCircularImageView1;
+    CircularImageView mCircularImageView2;
+    CircularImageView mCircularImageView3;
+    CircularImageView mCircularImageView4;
+    CircularImageView mCircularImageView5;
 
 
     @Override
@@ -31,22 +36,45 @@ public class GroupImageFragment extends BaseFragment {
     @Override
     protected void initViews(View view) {
         super.initViews(view);
-//        mCircleImnageView = (CircleImnageView) view.findViewById(R.id.circle_image);
-        mGroupImageView= (GroupImageView) view.findViewById(R.id.group_image);
-//        mCircleImnageView.setVisibility(View.GONE);
+        Bitmap avatar1 = BitmapFactory.decodeResource(getResources(), R.mipmap.headshow1);
+        Bitmap avatar2 = BitmapFactory.decodeResource(getResources(), R.mipmap.headshow2);
+        Bitmap avatar3 = BitmapFactory.decodeResource(getResources(), R.mipmap.headshow3);
+        Bitmap avatar4 = BitmapFactory.decodeResource(getResources(), R.mipmap.headshow4);
+        Bitmap avatar5 = BitmapFactory.decodeResource(getResources(), R.mipmap.headshow5);
+
+        mBmps1.add(avatar1);
+
+        mBmps2.add(avatar1);
+        mBmps2.add(avatar2);
+
+        mBmps3.add(avatar1);
+        mBmps3.add(avatar2);
+        mBmps3.add(avatar3);
+
+        mBmps4.add(avatar1);
+        mBmps4.add(avatar2);
+        mBmps4.add(avatar3);
+        mBmps4.add(avatar4);
+
+        mBmps5.add(avatar1);
+        mBmps5.add(avatar2);
+        mBmps5.add(avatar3);
+        mBmps5.add(avatar4);
+        mBmps5.add(avatar5);
 
 
+        mCircularImageView1 = (CircularImageView) view.findViewById(R.id.circularImageView1);
+        mCircularImageView2 = (CircularImageView) view.findViewById(R.id.circularImageView2);
+        mCircularImageView3 = (CircularImageView) view.findViewById(R.id.circularImageView3);
+        mCircularImageView4 = (CircularImageView) view.findViewById(R.id.circularImageView4);
+        mCircularImageView5 = (CircularImageView) view.findViewById(R.id.circularImageView5);
+
+        mCircularImageView1.setImageBitmaps(mBmps1);
+        mCircularImageView2.setImageBitmaps(mBmps2);
+        mCircularImageView3.setImageBitmaps(mBmps3);
+        mCircularImageView4.setImageBitmaps(mBmps4);
+        mCircularImageView5.setImageBitmaps(mBmps5);
 
     }
 
-    @Override
-    protected void initData() {
-        super.initData();
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.image_06);
-//        mCircleImnageView.setImageBitmap(bitmap);
-        List<Bitmap> bitmaps=new ArrayList<>();
-        bitmaps.add(bitmap);
-        mGroupImageView.setBitmaps(bitmaps);
-
-    }
 }
