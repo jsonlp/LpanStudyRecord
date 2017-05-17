3a64f62540b8570f123d39e0c506f8852d809ecc
lpan <353871489@qq.com>
2017-05-17 19:28:37 +0800

    base

---
+++ app/src/main/java/com/lpan/study/fragment/VideoRecyclerFragment.java
@@ -0,0 +1,17 @@
+package com.lpan.study.fragment;
+
+import com.test.lpanstudyrecord.R;
+
+/**
+ * Created by lpan on 2017/5/17.
+ */
+
+public class VideoRecyclerFragment extends BaseFragment {
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.fragment_video_recycler;
+    }
+
+
+}
