6cc19431228aadf9d00bdcb0eaacad78ad6677c0
liaopan <353871489@qq.com>
2017-10-20 16:26:41 +0800

    merge

--- app/src/main/java/com/lpan/study/fragment/customviewtest/Graphics2Dfragment.java
+++ app/src/main/java/com/lpan/study/fragment/customviewtest/Graphics2Dfragment.java
@@ -1,13 +1,28 @@
 package com.lpan.study.fragment.customviewtest;
 
+import android.widget.LinearLayout;
+
 import com.lpan.study.fragment.base.ButterKnifeFragment;
+import com.lpan.study.view.BallMoveView;
 import com.test.lpanstudyrecord.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import butterknife.BindView;
 
 /**
  * Created by lpan on 2017/10/20.
  */
 
 public class Graphics2Dfragment extends ButterKnifeFragment {
+
+    @BindView(R.id.layout)
+    LinearLayout mContainer;
+
+
+    @BindView(R.id.ball_move_view)
+    BallMoveView mBallView;
 
     @Override
     protected int getLayoutResource() {
@@ -24,4 +39,14 @@ public class Graphics2Dfragment extends ButterKnifeFragment {
         return "Graphics 2D";
     }
 
+    @Override
+    protected void initData() {
+        super.initData();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mBallView.postInvalidate();
+            }
+        }, 200, 50);
+    }
 }
---
+++ app/src/main/java/com/lpan/study/view/BallMoveView.java
@@ -0,0 +1,80 @@
+package com.lpan.study.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.lpan.study.utils.Log;
+
+/**
+ * Created by lpan on 2017/10/20.
+ */
+
+public class BallMoveView extends View {
+
+    private Paint mPaint;
+
+    private int x;
+
+    private int y = 100;
+
+    private int RADIUS = 30;
+
+    private boolean flag = false;
+
+    public BallMoveView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public BallMoveView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+
+    }
+
+    public BallMoveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+
+    }
+
+    public BallMoveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.RED);
+
+        x = RADIUS;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawCircle(x, y, RADIUS, mPaint);
+
+        int width = this.getMeasuredWidth();
+
+        if (x <= RADIUS) {
+            flag = true;
+        }
+
+        if (x >= width - RADIUS) {
+            flag = false;
+        }
+
+        x = flag ? x + 5 : x - 5;
+        if (Log.DEBUG) {
+            Log.d("BallMoveView", "onDraw--------x=" + x);
+        }
+    }
+}
