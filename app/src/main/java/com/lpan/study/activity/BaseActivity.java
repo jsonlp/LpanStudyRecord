c86da5396dd03ae417d9f5b3e3fb71453f8ef34d
liaopan <353871489@qq.com>
2017-11-14 16:45:43 +0800

    merge

--- app/src/main/java/com/lpan/study/activity/BaseActivity.java
+++ app/src/main/java/com/lpan/study/activity/BaseActivity.java
@@ -1,7 +1,5 @@
 package com.lpan.study.activity;
 
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
@@ -41,15 +39,6 @@ public abstract class BaseActivity extends FragmentActivity {
         return R.layout.activity_base;
     }
 
-    @Override
-    public Resources getResources() {
-        //解决更换字体后UI混乱 不知道是否有效
-        Resources res = super.getResources();
-        Configuration config=new Configuration();
-        config.setToDefaults();
-        res.updateConfiguration(config,res.getDisplayMetrics() );
-        return res;
-    }
 
     public void toastShort(String message){
         if (!TextUtils.isEmpty(message)) {
